fd22440777468d318e97643837acbe1b7f428165
Braxton Bowman <10923641@my.uvu.edu>
2022-04-27 00:39:41 -0600

    Completed "Lesson 6.1" parts 1 and 2

--- DrivingSim/Assets/Scripts/PlayerController.cs
+++ DrivingSim/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 5.0f;
-    private float turnSpeed = 45.0f;
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] float turnSpeed = 45.0f;
     private float horizontalInput;
     private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //Player Inputs
         horizontalInput = Input.GetAxis("Horizontal");
